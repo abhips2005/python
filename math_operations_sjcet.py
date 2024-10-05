64231aae4cdf3ade604351124d297d5fc14145c3
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-05 15:56:50 +0530

    Add files via upload
    Programme done with the use of math module

---
+++ math_operations_sjcet.py
@@ -0,0 +1,14 @@
+'''
+Name:Abhijith S
+Project: Operations on math module
+Date: 05.10.2024
+Version: 1.0
+'''
+import math
+number=int(input("Enter a number:"))
+square=math.sqrt(number)
+fact=math.factorial(number)
+power=math.pow(number,2)
+print(f"Square root of {number}: {square}")
+print(f"Factorial of {number}: {fact}")
+print(f"{number} raised to the power of 2: {power} ")
