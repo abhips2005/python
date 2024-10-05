85d5a10474e5185801f887ee3f315f6267736582
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-05 15:58:11 +0530

    Add files via upload
    Output the name and age entered by the user

---
+++ name_and_age_sjcet.py
@@ -0,0 +1,10 @@
+'''
+Name:Abhijith S
+Project: Output the name and age entered by the user
+Date: 05.10.2024
+Version: 1.0
+'''
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+print(f"Hello, {name}!, you are {age} years old.")
