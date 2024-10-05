b089f38c2b887f079e1b55382de7468603de10fa
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-05 16:05:00 +0530

    Add files via upload
    Twinkle Twinkle Little Star 
    Line alignment in python

---
+++ twinkle_twinkle_sjcet.py
@@ -0,0 +1,14 @@
+'''
+Name:Abhijith S
+Project: Twinkle Twinkle Little Star
+Date: 05.10.2024
+Version: 1.0
+'''
+
+
+print("Twinkle, twinkle, little star, \n\t\t"
+      "How I wonder what you are! \n\t\t\t\t"
+      "Up above the world so high, \n\t\t\t\t"
+      "Like a diamond in the sky.\n"
+      "Twinkle, twinkle, little star,\n\t\t"
+      "How I wonder what you are")
