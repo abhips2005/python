26f010e6cf4f7cb635d7da7131334e1226880c6e
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-05 16:02:58 +0530

    Add files via upload
    String extraction and concatenation using python

---
+++ string_extraction_sjcet.py
@@ -0,0 +1,12 @@
+'''
+Name:Abhijith S
+Project: String extraction and addition
+Date: 05.10.2024
+Version: 1.0
+'''
+
+var="Hello World!"
+str1=var[:5]
+str2=" Everyone!"
+str3=str1+str2
+print(str3)
