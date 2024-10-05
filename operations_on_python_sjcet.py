318fa96a1e67989e939998fc45cc0c9e9e3d7bf7
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-05 15:59:29 +0530

    Add files via upload
    Basic operations on python

---
+++ operations_on_python_sjcet.py
@@ -0,0 +1,20 @@
+'''
+Name:Abhijith S
+Project: Operations on python
+Date: 05.10.2024
+Version: 1.0
+'''
+
+number1=10
+number2=5
+sum=number1+number2
+division=number1/number2
+grater_than_b=(number1 > number2)
+a_b_equal=(number1 == number2)
+logical_and=(number1 > number2) and (number2 < number1)
+logical_or=(number1 > number2) or (number2 > number1)
+print(f"Sum:{sum},Division:{division}")
+print(f"Is grater than b?:{grater_than_b}")
+print(f"Are a and b equal?:{a_b_equal}")
+print(f"Logical AND:{logical_and}")
+print(f"Logical OR:{logical_or}")
