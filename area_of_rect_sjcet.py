3fddc7514d3840d3cd7b51d24927d0dcb1dde605
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-05 15:55:28 +0530

    Add files via upload
    Programme to find the area of a rectangle

---
+++ area_of_rect_sjcet.py
@@ -0,0 +1,10 @@
+'''
+Name:Abhijith S
+Project: Find the area of the rectangle
+Date: 01.10.2024
+Version: 1.0
+'''
+length=int(input("Enter the length of the rectangle:"))
+breadth=int(input("Enter the breadth of the rectangle:"))
+area=length*breadth
+print("The area of the rectangle:",area)
