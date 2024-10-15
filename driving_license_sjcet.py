d3be2c4da82653d83664e9be42016fe20a462125
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:34:37 +0530

    Add files via upload
    To check whether a person is eligible for driving license or not

---
+++ driving_license_sjcet.py
@@ -0,0 +1,12 @@
+'''
+Name:Abhijith S
+Project: To check whether a person is eligible for driving license or not
+Date: 15.10.2024
+Version: 1.0
+'''
+
+age=int(input("Enter your age: "))
+if age>=18:
+    print("You are eligible for driving license")
+else:
+    print('You are not eligible for driving license')
