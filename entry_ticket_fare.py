e4cf13b8c2b2b9398a6d976b192d485caa89fa3d
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-15 15:12:42 +0530

    Add files via upload
    To calculate the entry ticket fare according to age

---
+++ entry_ticket_fare.py
@@ -0,0 +1,14 @@
+'''
+Name:Abhijith S
+Project: To calculate the discount based on the bill amount
+Date: 15.10.2024
+Version: 1.0
+'''
+
+age=int(input("Enter your age: "))
+if age<10:
+    print(f"The fare is 7 rs")
+elif 10 <= age < 60:
+    print(f"The fare is 10 rs")
+else:
+    print("The fare is 5 rs")
