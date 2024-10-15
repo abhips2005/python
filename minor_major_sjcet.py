d9392dbdaf06040864caacf6cfc9e15c6712d186
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:16:33 +0530

    Add files via upload
    To check whether a person is major or minor

---
+++ minor_major_sjcet.py
@@ -0,0 +1,12 @@
+'''
+Name:Abhijith S
+Project: To check whether a person is major or minor
+Date: 15.10.2024
+Version: 1.0
+'''
+
+age=int(input("Enter your age: "))
+if age>=18:
+    print("You are a major")
+else:
+    print('You are a minor')
