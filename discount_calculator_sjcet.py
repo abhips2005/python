f23d73ab492726a74e83a38fcff0b5d4c1eb2890
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-15 15:00:42 +0530

    Add files via upload
    To calculate the discount based on the bill amount

---
+++ discount_calculator_sjcet.py
@@ -0,0 +1,18 @@
+'''
+Name:Abhijith S
+Project: To calculate the discount based on the bill amount
+Date: 05.10.2024
+Version: 1.0
+'''
+
+bill=int(input("Enter your total bill amount: "))
+if bill>500:
+    discount=(bill/100)*20
+    to_pay=bill-discount
+    print(f"Total to pay after discount is Rs. {to_pay}")
+elif bill>=200:
+    discount = (bill / 100)*10
+    to_pay = bill - discount
+    print(f"Total to pay after discount is Rs. {to_pay}")
+else:
+    print(f"The total bill to pay is {bill}")
