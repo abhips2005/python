6bf2dbb8079b09b0cb7920046ee4bf2d4cbbd311
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:46:55 +0530

    Add files via upload
    To check whether a number is divisible by 5

---
+++ num_divisible_by_five.py
@@ -0,0 +1,12 @@
+'''
+Name:Abhijith S
+Project: To check whether a number is divisible by 5
+Date: 05.10.2024
+Version: 1.0
+'''
+
+num = int(input("Enter a number: "))
+if num%5==0:
+    print(f"The number {num} is divisible by 5")
+else:
+    print(f"The number {num} is not divisible by 5")
