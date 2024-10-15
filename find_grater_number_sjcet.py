ea1e61524430ed9b3dc8fc7a40b29ca19b65ace7
abhips2005 <abhijiths2028@cs.sjcetpalai.ac.in>
2024-10-15 14:31:22 +0530

    Add files via upload
    To find the grater number

---
+++ find_grater_number_sjcet.py
@@ -0,0 +1,15 @@
+'''
+Name:Abhijith S
+Project: To find the grater number
+Date: 15.10.2024
+Version: 1.0
+'''
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1>num2:
+    print(f"Nuber {num1} is grater than number {num2}")
+elif num2>num1:
+    print(f"Number {num2} is grater than number {num1}")
+else:
+    print("Both the numbers are equal")
